211aae376f6ea8f63013d8860fa4f6220b219332
TBYT <TBYTBiz@googlegroups.com>
2022-03-14 05:09:22 -0700

    CSS Design Implemented, Profile Pictures and Session Navbar
    Milestone 5 changes, also fixed http protocol error bug.

---
+++ SmallRatings/SmallRatings/Models/ProInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SmallRatings.Models
+{
+    public class ProInfo
+    {
+        public int ProID { get; set; }
+        public int UserID { get; set; }
+
+        [Display(Name = "Business Name")]
+        [Required(ErrorMessage = "Please enter your business name.")]
+        public string ProName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Please enter your last name.")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "Please enter a username.")]
+        public string Username { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Please enter your email.")]
+        public string Email { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone Number")]
+        [Required(ErrorMessage = "Please enter a phone number.")]
+        public int Number { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Please enter a password.")]
+        public string Password { get; set; }
+    }
+}
